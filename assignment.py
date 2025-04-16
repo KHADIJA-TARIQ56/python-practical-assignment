5967c3cd859cc78b265a50381cfe28bbbc0e3341
KHADIJA-TARIQ56 <khadijahtariq26@gmail.com>
2025-04-16 20:47:35 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,26 @@
+# print hello world
+print("hello world!")
+# print x value
+x=1
+print(x)
+# give input and take the output
+input("enter your name")
+# enter a array
+fruits=["apple","banana","grapes"]
+print(fruits)
+# adding two numbers
+a=5
+b=6
+c=a+b
+print(c)
+# enter a number
+x=input("enter a number")
+print(x)
+
+# 
+a=('abc','xyz','123')
+print(a)
+# 
+a=(1)=2
+# 
+a[1]=2
